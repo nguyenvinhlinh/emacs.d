79709ee9207ca148ad3fb3a5414ebe7f38c6fe81
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2018-03-09 11:29:02 +0700

    M-` to toggle truncate lines, change font size to 100

--- config/keybinding.el
+++ config/keybinding.el
@@ -1,2 +1,4 @@
 (global-set-key (kbd "C-c g") 'magit-status)
+(global-unset-key (kbd "M-`"))
+(global-set-key (kbd "M-`") 'toggle-truncate-lines)
 (provide 'keybinding)
--- config/theme.el
+++ config/theme.el
@@ -2,5 +2,6 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
+(set-face-attribute 'default nil :height 100)
 
 (provide 'theme)
