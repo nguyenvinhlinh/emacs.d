dec148347353c6a0697f43b0c410bda5aa905fbb
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2018-04-12 11:19:50 +0700

    change font size from 100 to 80

--- config/theme.el
+++ config/theme.el
@@ -2,6 +2,6 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
-(set-face-attribute 'default nil :height 100)
+(set-face-attribute 'default nil :height 80)
 
 (provide 'theme)
