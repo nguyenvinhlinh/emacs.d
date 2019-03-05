ba3a7c05ba6dbf30215745bb2f25f7f262f28cde
Nguyen Vinh Linh <nguyenvinhlinh@localhost.localdomain>
2019-03-05 22:10:18 +0700

    add emacs lisp script emacs-dependency-installation.el

---
+++ emacs-dependency-installation.el
@@ -0,0 +1,17 @@
+(package-initialize)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-refresh-contents)
+
+(setq package_list
+      (quote (markdown-mode smart-mode-line-powerline-theme smart-mode-line
+                            groovy-mode json-mode smartparens yasnippet
+                            exec-path-from-shell tide company smex ido-vertical-mode
+                            projectile magit dracula-theme emmet-mode elixir-mode web-mode))
+      )
+
+(dolist (package_name package_list)
+  (package-install package_name)
+  )
+
+(provide 'emacs-dependency-install)
