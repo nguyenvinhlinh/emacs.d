0ca2480e224450215e9e04a49b5557a70ffc1333
Nguyen Vinh Linh <linh.nguyen@ttekglobal.com>
2020-03-06 20:55:19 +0700

    work

--- config/linh-text-navigation-mode.el
+++ config/linh-text-navigation-mode.el
@@ -1,24 +1,28 @@
 (defvar csgo-navigation-map nil "Keymap for `csgo-navigation-mode")
 (defvar csgo-navigation-map-activated nil "An indicator for `csgo-navigation-map")
 
-
 (progn
   (setq csgo-navigation-map (make-sparse-keymap))
-  (define-key csgo-navigation-map (kbd "w") 'previous-line)
-  (define-key csgo-navigation-map (kbd "a") 'backward-char)
-  (define-key csgo-navigation-map (kbd "s") 'next-line)
-  (define-key csgo-navigation-map (kbd "d") 'forward-char)
+  (define-key csgo-navigation-map (kbd "w") (lambda () (interactive) (previous-line 3)))
+  (define-key csgo-navigation-map (kbd "a") (lambda () (interactive) (backward-char 3)))
+  (define-key csgo-navigation-map (kbd "s") (lambda () (interactive) (next-line 3)))
+  (define-key csgo-navigation-map (kbd "d") (lambda () (interactive) (forward-char 3)))
+
+  (define-key csgo-navigation-map (kbd "W") 'previous-line)
+  (define-key csgo-navigation-map (kbd "A") 'backward-char)
+  (define-key csgo-navigation-map (kbd "S") 'next-line)
+  (define-key csgo-navigation-map (kbd "D") 'forward-char)
+
+  (define-key csgo-navigation-map (kbd "M-d") 'forward-word)
+  (define-key csgo-navigation-map (kbd "M-a") 'backward-word)
   )
 
+
 (define-minor-mode csgo-navigation-mode
-  "This mode navigate cursor using CSGO Style"
-  :INIT-VALUE nil
-  :LIGHTER "csgo-cursor-navigation"
-  :KEYMAP csgo-navigation-map
-
-  (progn
-
-    )
+  "csgo-navigation-mode helps"
+  nil
+  "CSGO"
+  csgo-navigation-map
 )
 
 (defun csgo-navigation-toggle ()
@@ -37,22 +41,6 @@
     )
   )
 
-;; (use-local-map csgo-navigation-map)
-
-;; (current-local-map)
-
-(current-local-map)
 
 
-(use-local-map global-map)
-(use-local-map csgo-navigation-map)
-(setq overriding-local-map csgo-navigation-map)
-(setq overriding-local-map nil)
-
-
-
-minor-mode-alist
-
-(csgo-navigation-toggle)
-
-(self-insert-command 9)
+(previous-line 3)
